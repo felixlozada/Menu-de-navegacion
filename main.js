e0b6f341fe0e6715171139c0966e933e4e62c0f3
unknown <felix lozada>
2020-06-17 18:07:40 -0400

    Subiendo archivos

---
+++ main.js
@@ -0,0 +1,21 @@
+let menuIcon = document.querySelector('.menu-icon');
+let menu = document.querySelector('nav');
+
+menuIcon.addEventListener('click', ()=>{
+	menu.classList.toggle('oculto');
+});
+
+let menuItems = document.querySelectorAll('.menu li');
+
+menuItems.forEach((item)=>{
+	item.addEventListener('click', ()=>{
+
+		for(i=0; i < menuItems.length; i++){
+			if(menuItems[i].className == 'active'){
+				menuItems[i].classList.remove('active');
+			}
+		}
+		
+		item.classList.add('active');
+	});
+});
